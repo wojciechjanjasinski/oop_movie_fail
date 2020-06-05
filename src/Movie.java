adfadb7178da60d0a4d9dea6756888633fccc6b5
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-05 12:30:14 +0200

    Add: .gitigonre, int rating and boolean oscar in Movie.java and clear one print function

--- src/Movie.java
+++ src/Movie.java
@@ -2,4 +2,6 @@ class Movie {
     String title;
     String description;
     String director;
+    int rating;
+    boolean oscar;
 }
--- src/MovieLibrary.java
+++ src/MovieLibrary.java
@@ -6,12 +6,13 @@ class MovieLibrary {
                 "z niedowładem kończyn, który staje się miliarderem i bohaterem wojny w Wietnamie.";
         movie.director = "Robert Zemeckis";
         movie.rating = 9;
+        movie.oscar = true;
 
         System.out.println("Informacje o filmie:");
         System.out.printf("Tytuł: %s\n", movie.title);
         System.out.printf("Opis: %s\n", movie.description);
         System.out.printf("Reżyser: %s\n", movie.director);
-        System.out.printf("Ocena: %f/10\n", movie.rating);
-        System.out.printf("Czy otrzymał Oskara: %s\n", movie.oscars);
+        System.out.printf("Ocena: %s\n", movie.rating);
+        System.out.printf("Czy otrzymał Oskara: %s\n", movie.oscar);
     }
 }
